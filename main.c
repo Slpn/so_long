4e017e7de06c1f3b66bc5488e9db45d146681f37
Shaan NARAIN <snarain@e1r8p5.42.fr>
2021-08-12 19:05:09 +0200

    tmp

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: snarain <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 19:19:06 by snarain           #+#    #+#             */
-/*   Updated: 2021/08/11 18:17:12 by snarain          ###   ########.fr       */
+/*   Updated: 2021/08/12 18:55:44 by snarain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,41 @@
 
 #define MLX_ERROR 1
 
+int	colors(t_mlx *data)
+{
+	if (data->win != NULL)
+		mlx_pixel_put(data->mlx, data->win, 
+				WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, RED_PIXEL);
+	return (0);
+}
+
+int	handle_keypress(int keysym, t_mlx *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx, data->win);
+		data->win = NULL;
+	}
+	return (0);
+}
+
 int main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	t_mlx	data;
 
-	mlx_ptr = mlx_init();
-	if (mlx_ptr == NULL)
+	data.mlx = mlx_init();
+	if (data.mlx == NULL)
 		return (MLX_ERROR);
-	win_ptr = mlx_new_window(mlx_ptr, 600, 300, "My first window!");
-	if (win_ptr == NULL)
+	data.win = mlx_new_window(data.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, 
+			"My first window!");
+	if (data.win == NULL)
 	{
-		free(win_ptr);
+		free(data.win);
 		return (MLX_ERROR);
 	}
-	mlx_loop(mlx_ptr);
-	free(mlx_ptr);
+	mlx_loop_hook(data.mlx, &colors, &data);
+	mlx_hook(data.win, KeyPress, KeyPressMask, &handle_keypress, &data);
+	mlx_loop(data.mlx);
+	free(data.mlx);
 	return (0);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: snarain <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 21:53:42 by snarain           #+#    #+#             */
-/*   Updated: 2021/08/11 18:37:19 by snarain          ###   ########.fr       */
+/*   Updated: 2021/08/12 18:47:32 by snarain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 # include "mlx/mlx.h"
 # include <stdlib.h>
 # include <stdio.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+# define RED_PIXEL 0xFF0000
+
+# define WINDOW_WIDTH	600
+# define WINDOW_HEIGHT	300
 
 typedef struct s_mlx
 {
